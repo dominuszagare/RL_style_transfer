137c7fb6990d3c6eccbd39599487af6fa548bce0
ddominik33 <ddominik33@gmail.com>
2023-05-11 21:19:05 +0200

    init

---
+++ RunContainer.sh
@@ -0,0 +1,19 @@
+docker run --gpus all --ipc=host -v /mnt/d/REPO-2/TextRL:/textRL --ulimit memlock=-1 --ulimit stack=67108864 -it -p 8888:8888 --name pytorch --rm nvcr.io/nvidia/pytorch:23.04-py3
+
+jupyter notebook .
+
+cd ../sig
+docker cp ./ pytorch:workspace/sig
+docker cp pytorch:workspace/sig ./
+
+https://github.com/voidful/TextRL#initialize-agent-and-environment
+
+
+pip install pfrl@git+https://github.com/voidful/pfrl.git
+pip install textrl
+
+OR
+
+pip install -e .
+
+
